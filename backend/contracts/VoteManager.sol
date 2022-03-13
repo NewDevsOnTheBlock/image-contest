1b6b3faa7025bed2fd431177281a3f71d24e542b
collins-k <cyrille_08kamgaing@yahoo.fr>
2022-03-13 17:38:11 +0100

    Update vote manager contract

--- backend/contracts/VoteManager.sol
+++ backend/contracts/VoteManager.sol
@@ -2,42 +2,42 @@
 pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
-import "./candidate.sol";
+import { AddressHasAlreadyVoted } from "./helpers.sol";
 
 contract VoteManager {
 
-
     enum VoteStatus{CREATED, OPEN, CLOSED}
 
-    struct C_Canditate {
-        Candidate _candidate;
+    struct Candidate {
         uint _totalVote;
         string _name;
-        bytes32 imageHash;
-        bool hasAlreadyVoted;
+        string _imageHash;
     }
 
     VoteManager.VoteStatus _status;
     address internal winner;
 
-    mapping(address => C_Canditate) public candidates;
+    mapping(address => Candidate) public candidates;
     mapping(address => bool) public hasAddressVoted;
 
     event VoteStatusEvent(address _candidateAddress, uint _status);
-    event Voted(address _candidateAddress, address _voterAddress, uint _totalVote);
-    event Winner(C_Canditate winner);
-    event candidateCreated(address canditate, string name);
+    event Voted(address indexed _candidateAddress, address indexed _voterAddress, uint _totalVote);
+    event candidateCreated(address indexed canditateAddress, string name);
 
-    function createCanditate(string memory _name) public {
-        Candidate candidate = new Candidate(this);
-        address _address = address(candidate);
-        candidates[_address]._candidate = candidate;
-        candidates[_address]._name = _name;
+    function registerCanditate(string memory _name, string memory _imageHash) public {
+        Candidate memory newCandidate = Candidate(0, _name, _imageHash);
+        address _address = address(msg.sender);
+        candidates[_address] = newCandidate;
 
         emit candidateCreated(_address, _name);
     }
 
     function vote(address _forCandidate) public {
+        if (hasAddressVoted[msg.sender])
+            revert AddressHasAlreadyVoted({
+            voter: msg.sender
+            });
+
         hasAddressVoted[msg.sender] = true;
         candidates[_forCandidate]._totalVote += 1;
 
@@ -52,7 +52,7 @@ contract VoteManager {
         return candidates[_forCandidate]._totalVote;
     }
 
-    function getWinner() public {
-        emit Winner(candidates[winner]);
+    function getWinner() public view returns(Candidate memory){
+        return candidates[winner];
     }
 }
---
+++ backend/contracts/helpers.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.7;
+
+/**
+ * The current address has already voted
+ * @param voter address.
+ */
+    error AddressHasAlreadyVoted(address voter);
