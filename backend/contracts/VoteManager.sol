e88ae53e8b0d8b145ec685ceb7db3d4c5e1d6e9a
XamHans <muellerjohannes93@gmail.com>
2022-03-15 20:53:46 +0100

    added tests

--- backend/contracts/VoteManager.sol
+++ backend/contracts/VoteManager.sol
@@ -3,21 +3,26 @@ pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
 import { AddressHasAlreadyVoted } from "./helpers.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract VoteManager {
 
     enum VoteStatus{CREATED, OPEN, CLOSED}
 
     struct Candidate {
-        uint _totalVote;
-        string _name;
-        string _imageHash;
+        uint id;
+        uint totalVote;
+        string name;
+        string imageHash;
     }
+    using Counters for Counters.Counter;
+    Counters.Counter private candidatesIds;
 
     VoteManager.VoteStatus _status;
     address internal winner;
 
     mapping(address => Candidate) public candidates;
+    mapping (uint=> address ) private accounts;
     mapping(address => bool) public hasAddressVoted;
 
     event VoteStatusEvent(address _candidateAddress, uint _status);
@@ -25,31 +30,46 @@ contract VoteManager {
     event candidateCreated(address indexed canditateAddress, string name);
 
     function registerCanditate(string memory _name, string memory _imageHash) public {
-        Candidate memory newCandidate = Candidate(0, _name, _imageHash);
+        //check if candidate already registered
+        require(candidates[msg.sender].id == 0 || candidates[msg.sender].id > 0, "Candidate already registered");
+        candidatesIds.increment();
+        uint candidateId = candidatesIds.current();
+        Candidate memory newCandidate = Candidate(candidateId, 0, _name, _imageHash);
         address _address = address(msg.sender);
         candidates[_address] = newCandidate;
-
+        accounts[candidateId] = msg.sender;
         emit candidateCreated(_address, _name);
     }
 
+      /* fetches all candidates */
+    function fetchCandidates() public view returns ( Candidate[] memory) {
+        uint itemCount = candidatesIds.current();
+
+        Candidate[] memory candidatesArray = new Candidate[](itemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            uint currentId = i + 1;
+            Candidate storage currentItem = candidates[accounts[currentId]];
+            candidatesArray[i] = currentItem;
+        }
+        return candidatesArray;
+    }
+
+
     function vote(address _forCandidate) public {
-        if (hasAddressVoted[msg.sender])
-            revert AddressHasAlreadyVoted({
-            voter: msg.sender
-            });
+        require(hasAddressVoted[msg.sender], "Candidate has already voted");
 
         hasAddressVoted[msg.sender] = true;
-        candidates[_forCandidate]._totalVote += 1;
+        candidates[_forCandidate].totalVote += 1;
 
-        emit Voted(_forCandidate, msg.sender, candidates[_forCandidate]._totalVote);
+        emit Voted(_forCandidate, msg.sender, candidates[_forCandidate].totalVote);
 
-        if (candidates[_forCandidate]._totalVote > candidates[winner]._totalVote){
+        if (candidates[_forCandidate].totalVote > candidates[winner].totalVote){
             winner =  _forCandidate;
         }
     }
 
     function getTotalVote(address _forCandidate) public view returns(uint) {
-        return candidates[_forCandidate]._totalVote;
+        return candidates[_forCandidate].totalVote;
     }
 
     function getWinner() public view returns(Candidate memory){
--- backend/test/index.ts
+++ backend/test/index.ts
@@ -1,19 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("VoteManager", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  it("Should create a new candidate", async function () {
+    const VoteManager = await ethers.getContractFactory("VoteManager");
+    const votemanager = await VoteManager.deploy();
+    await votemanager.deployed();
+    await votemanager.registerCanditate('Hannes', 'QmXMxbkj4Wbx8qn1PDZ4v91F9u6Y4YqHkieXmZDpPFKU4T')
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const candidates = await votemanager.fetchCandidates();
+    console.log(candidates)
+    expect(candidates[0].name).to.equal("Hannes")
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Should faild create a duplcicate candidate", async function () {
+    const VoteManager = await ethers.getContractFactory("VoteManager");
+    const votemanager = await VoteManager.deploy();
+    await votemanager.deployed();
+    await votemanager.registerCanditate('Hannes', 'QmXMxbkj4Wbx8qn1PDZ4v91F9u6Y4YqHkieXmZDpPFKU4T')
+    expect(() => votemanager.registerCanditate('Hannes', 'QmXMxbkj4Wbx8qn1PDZ4v91F9u6Y4YqHkieXmZDpPFKU4T')).to.throw("Candidate already registered");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
